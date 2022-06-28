469542443c69aafe2e63d30a548b941bf90609b9
Fortunize <96587252+Fortunize@users.noreply.github.com>
2022-06-28 08:07:02 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/app/mindlistic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.mindlistic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/dash_board.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DashBoard extends StatefulWidget {
+  DashBoard({Key? key}) : super(key: key);
+
+  @override
+  State<DashBoard> createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
